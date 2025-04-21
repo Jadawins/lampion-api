09a57540e89ee9e0a4653d04e9bd5b01d3f2cd00
Jadawins <jadawins@gmail.com>
2025-04-21 08:23:59 +0200

    ajout passe tour

--- PasserTour/index.js
+++ PasserTour/index.js
@@ -28,7 +28,17 @@ module.exports = async function (context, req) {
 
     index = (index + 1) % ordre.length;
     data.indexTour = index;
-
+    const timestamp = new Date().toISOString();
+    const previousIndex = (index - 1 + ordre.length) % ordre.length;
+    const entite = ordre[previousIndex]; // celui qui vient de jouer (ou passer)
+    
+    if (!data.logCombat) data.logCombat = [];
+    
+    data.logCombat.push({
+      type: "passer_tour",
+      auteur: entite?.pseudo || entite?.nom || "inconnu",
+      timestamp
+    });
     await blobClient.upload(JSON.stringify(data), Buffer.byteLength(JSON.stringify(data)), {
       overwrite: true
     });
