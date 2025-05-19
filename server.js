20e540455d9394571b242940a983ec3691c73612
Jadawins <jadawins@gmail.com>
2025-05-19 12:57:05 +0200

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+throw new Error('TEST - Erreur levée à l’import du routeur GetCategories');
 console.log('DEBUG MONGO_URI:', process.env.MONGO_URI);
 const express = require('express');
 const fs = require('fs');
