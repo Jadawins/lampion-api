00f318ca6bd2a27ca0a5574ec371992e2a5b39a9
Jadawins <jadawins@gmail.com>
2025-05-19 09:53:28 +0200

    Créer le client Mongo DANS la route

--- GetCategories/index.js
+++ GetCategories/index.js
@@ -8,20 +8,22 @@ const collectionName = 'categories';
 
 // Route GET pour récupérer les catégories
 router.get('/', async (req, res) => {
-  console.log('Route /api/GetCategories appelée');
+  console.log('>> ROUTE /api/GetCategories EXECUTED <<');
+
+  const client = new MongoClient(process.env.MONGO_URI);
   try {
     await client.connect();
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
+    const db = client.db('myrpgtable');
+    const collection = db.collection('categories');
     const categories = await collection.find().toArray();
     res.status(200).json(categories);
   } catch (error) {
-  console.error('Erreur lors de la récupération des catégories :', error.message, error.stack);
-  res.status(500).json({ error: error.message });
+    console.error('Erreur lors de la récupération des catégories :', error.message, error.stack);
+    res.status(500).json({ error: error.message });
   } finally {
-    // Pas besoin de fermer ici si tu utilises un client partagé
+    await client.close();
   }
 });
+
 
 module.exports = router;
